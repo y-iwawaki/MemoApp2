638c621a46907907484327330789d5feaad6ce69
y-iwawaki <iwawakiyong@gmail.com>
2025-01-12 10:24:02 +0900

    Markup commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,62 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = ():JSX.Element => {
     return(
+        <View style={styles.container}>
+         <View>
+            <View>
+             <Text>Memo App</Text>
+             <Text>ログアウト</Text> 
+            </View>
+         </View>     
+         <View>
+
         <View>
-         <Text>Hellow World</Text>
+            <View>
+                <Text>買い物リスト</Text>
+                <Text>2024年01月12日</Text>
+            </View>
+            <View>
+                <Text>XX</Text>
+            </View>
+         </View>
+        
+         <View>
+            <View>
+                <Text>買い物リスト</Text>
+                <Text>2024年01月12日</Text>
+            </View>
+            <View>
+                <Text>XX</Text>
+            </View>
+         </View>
+
+         <View>
+            <View>
+                <Text>買い物リスト</Text>
+                <Text>2024年01月12日</Text>
+            </View>
+            <View>
+                <Text>XX</Text>
+            </View>
+         </View>
+
+         </View>
+         <View>
+            <Text>+</Text>
+         </View>
         </View>
     )
  
 
 }
+
+const styles = StyleSheet.create({
+ container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+ }
+})
 
 export default Index
