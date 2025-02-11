3c7bfbfd99996d71646315506e2d9ee64670a465
y-iwawaki <iwawakiyong@gmail.com>
2025-02-11 14:31:36 +0900

    fire add

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -2,7 +2,7 @@ import {
     View, Text, TextInput,
     TouchableOpacity, StyleSheet } from 'react-native'
 import {Link, router} from 'expo-router'
-
+import { useState } from 'react'
 
 import Button from '../../components/Button'
 
@@ -12,13 +12,31 @@ const handlPress =(): void => {
 }
 
 const SignUp = (): JSX.Element => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <View style={styles.container}>
 
           <View style={styles.inner}>
             <Text style={styles.title}>Sign Up</Text>
-            <TextInput style={styles.input} value='Email address' />
-            <TextInput style={styles.input} value='Password' />
+            <TextInput
+             style={styles.input}
+             value={email}
+             onChangeText={(text) =>{setEmail(text)}}
+             autoCapitalize='none'
+             keyboardType='email-address'
+             placeholder='Email Address'
+             textContentType='emailAddress'
+            />
+            <TextInput
+             style={styles.input}
+             value={password}
+             onChangeText={(text) =>{setPassword(text)}}
+             autoCapitalize='none'
+             secureTextEntry
+             placeholder='Password'
+             textContentType='password'
+            />
             <Button label='Submit' onPress={handlPress}/>
             <View style={styles.footer}>
                 <Text style={styles.footerText}>Already registered?</Text>
